05d18cda2d9489e3ecfa994d170cc62d5e97e447
Anil <durananil@outlook.com>
2023-10-31 22:17:40 +0300

    add comment feature, return statements and remove semicolons

--- src/controllers/podcast.controller.js
+++ src/controllers/podcast.controller.js
@@ -1,22 +1,121 @@
-const prisma = require('../database/prisma');
+const prisma = require('../database/prisma')
 
 class PodcastController {
 
     static async getPodcasts(req, res, next) {
         try {
-            const podcasts = await prisma.podcast.findMany();
-            res.status(200).json(podcasts);
+            const podcasts = await prisma.podcast.findMany()
+            return res.status(200).json(podcasts)
         } catch (error) {
             res.status(500).json({
                 message: 'Unexpected error!'
-            });
+            })
+        }
+    }
+
+    static async getPodcastById(req, res, next) {
+        try {
+
+            const podcast = await prisma.podcast.findFirst({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            return res.status(200).json(podcast)
+
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Unexpected error!'
+            })
+        }
+    }
+    static async createPodcast(req, res, next) {
+        try {
+
+            const { title, description, imageUrl, podcastUrl, podcastListId } = req.body
+
+            const podcast = await prisma.podcast.create({
+                data: {
+                    title,
+                    description,
+                    imageUrl,
+                    podcastUrl,
+                    podcastList: {
+                        connect: {
+                            id: podcastListId
+                        }
+                    }
+                },
+            })
+            
+            return res.status(201).json(podcast)
+
+        } catch(error) {
+            return res.status(500).json({
+                message: 'Unexpected error!'
+            })
+        }
+    }
+
+    static async updatePodcast(req, res, next) {
+        try {
+
+            let willBeUpdatedFields = {}
+
+            if (req.body.title) {
+                willBeUpdatedFields.title = req.body.title
+            }
+            
+            if (req.body.description) {
+                willBeUpdatedFields.description = req.body.description
+            }
+
+            if (req.body.imageUrl) {
+                willBeUpdatedFields.imageUrl = req.body.imageUrl
+            }
+
+            if (req.body.podcastUrl) {
+                willBeUpdatedFields.podcastUrl = req.body.podcastUrl
+            }
+
+            const result = await prisma.podcast.update({
+                data: {
+                    ...willBeUpdatedFields
+                },
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            return res.status(200).json(result)
+
+        } catch(error) {
+            return res.status(500).json({
+                message: 'Unexpected error!'
+            })
+        }
+    }
+
+    static async deletePodcast(req, res, next) {
+        try {
+            const result = await prisma.podcast.delete({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            return res.status(200).json(result)
+
+        } catch(error) {
+            return res.status(500).json({
+                message: 'Unexpected error!'
+            })
         }
     }
 
     static async getLikedPodcasts(req, res, next) {
         try {
-            // const podcasts = await prisma.podcast.findMany()
-
             const podcasts = await prisma.podcast.findMany({
                 where: {
                     likedByUsers: {
@@ -27,71 +126,34 @@ class PodcastController {
                 }
             })
 
-            res.status(200).json(podcasts)
+            return res.status(200).json(podcasts)
         } catch(error) {
-            res.status(500).json({
+            return res.status(500).json({
                 message: 'Unexpected error!'
-            });
+            })
         }
     }
 
     static async likePodcast(req, res, next) {
         try {
-
-            // const result = await prisma.user.update({
-            //     data: {
-            //         likedPodcasts: {
-            //             connect: {
-            //                 podcastId_userId: {
-            //                     userId: req.user.id,
-            //                     podcastId: req.params.id
-            //                 }
-            //             }
-            //         }
-            //     },
-            //     where: {
-            //         id: req.user.id
-            //     }
-            // });
-
             const result = await prisma.podcastLikes.create({
                 data: {
                     podcastId: req.params.id,
                     userId: req.user.id
                 }
-            });
+            })
 
-            // const result = await prisma.podcastLikes.findMany();
-
-
-            res.status(200).json(result);
+            return res.status(200).json(result)
             
         } catch(error) {
-            console.log(error);
-            res.status(500).json({
+            return res.status(500).json({
                 message: 'Unexpected error!'
-            });
+            })
         }
     }
 
     static async unlikePodcast(req, res, next) {
         try {
-
-            // const result = await prisma.user.update({
-            //     data: {
-            //         likedPodcasts: {
-            //             disconnect: {
-            //                 podcastId_userId: {
-            //                     userId: req.user.id,
-            //                     podcastId: req.params.id
-            //                 }
-            //             }
-            //         }
-            //     },
-            //     where: {
-            //         id: req.user.id
-            //     }
-            // });
 
             const result = await prisma.podcastLikes.delete({
                 where: {
@@ -100,49 +162,91 @@ class PodcastController {
                         userId: req.user.id
                     }
                 }
-            });
+            })
 
-            res.status(200).json(result);
+            return res.status(200).json(result)
 
         } catch(error) {
-            console.log(error);
-            res.status(500).json({
+            return res.status(500).json({
                 message: 'Unexpected error!'
-            });
+            })
         }
     }
 
-    static async updatePodcast(req, res, next) {
+    static async getComments(req, res, next) {
         try {
+            const comments = await prisma.podcastComment.findMany({
+                where: {
+                    podcastId: req.params.id
+                }
+            })
 
+            return res.status(200).json(comments)
         } catch(error) {
-            res.status(500).json({
+            return res.status(500).json({
                 message: 'Unexpected error!'
-            });
+            })
         }
     }
 
-    static async deletePodcast(req, res, next) {
+    static async createComment(req, res, next) {
         try {
 
-            const result = await prisma.podcast.delete({
+            const comment = await prisma.podcastComment.create({
+                data: {
+                    content: req.body.content,
+                    userId: req.body.userId,
+                    podcastId: req.body.podcastId
+                }
+            })
+
+            return res.status(201).json(comment)
+
+        } catch(error) {
+            return res.status(500).json({
+                message: 'Unexpected error!'
+            })
+        }
+    }
+
+    static async updateComment(req, res, next) {
+        try {
+
+            const comment = await prisma.podcastComment.update({
+                data: {
+                    content: req.body.content
+                },
                 where: {
                     id: req.params.id
                 }
-            });
+            })
 
-            res.status(200).json({
-                message: 'Successfull!'
-            });
-
-
+            return res.status(200).json(comment)
         } catch(error) {
-            res.status(500).json({
+            return res.status(500).json({
                 message: 'Unexpected error!'
-            });
+            })
         }
     }
 
+    static async deleteComment(req, res, next) {
+        try {
+            const comment = await prisma.podcastComment.delete({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            return res.status(200).json(comment)
+        } catch(error) {
+            return res.status(500).json({
+                message: 'Unexpected error!'
+            })
+        }
+    }
+
+    
+
 }
 
-module.exports = PodcastController;
+module.exports = PodcastController
--- src/routes/podcasts.route.js
+++ src/routes/podcasts.route.js
@@ -1,17 +1,31 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
-const prisma = require('../database/prisma');
-const PodcastController = require('../controllers/podcast.controller');
+const express = require('express')
+const router = express.Router()
+const auth = require('../middlewares/auth')
+const prisma = require('../database/prisma')
+const PodcastController = require('../controllers/podcast.controller')
 
-router.get('/', auth, PodcastController.getPodcasts);
+router.get('/', auth, PodcastController.getPodcasts)
 
-router.get('/liked', auth, PodcastController.getLikedPodcasts);
+router.get('/:id', auth, PodcastController.getPodcastById)
 
-router.post('/:id/like', auth, PodcastController.likePodcast);
+router.post('/', auth, PodcastController.createPodcast)
 
-router.delete('/:id/unlike', auth, PodcastController.unlikePodcast);
+router.put('/:id', auth, PodcastController.updatePodcast)
 
-router.delete('/:id', auth, PodcastController.deletePodcast);
+router.delete('/:id', auth, PodcastController.deletePodcast)
 
-module.exports = router;
+router.get('/liked', auth, PodcastController.getLikedPodcasts)
+
+router.post('/:id/like', auth, PodcastController.likePodcast)
+
+router.delete('/:id/unlike', auth, PodcastController.unlikePodcast)
+
+router.get('/:id/comments', auth, PodcastController.getComments)
+
+router.post('/:id/comments', auth, PodcastController.createComment)
+
+router.put('/:id/comments/:commentId', auth, PodcastController.updateComment)
+
+router.delete('/:id/comments/:commentId', auth, PodcastController.deleteComment)
+
+module.exports = router
