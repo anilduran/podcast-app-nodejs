7ad7a4dc3954eb7a44e77dd9e31c3a5e08048f47
Anil <durananil@outlook.com>
2023-10-31 22:19:05 +0300

    add comment feature, return statements and remove semicolons

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/user.controller');
-const auth = require('../middlewares/auth');
+const express = require('express')
+const router = express.Router()
+const UserController = require('../controllers/user.controller')
+const auth = require('../middlewares/auth')
 
-router.get('/', auth, UserController.getUsers);
+router.get('/', auth, UserController.getUsers)
 
-router.get('/:id/podcast-lists', auth, UserController.getPodcastLists);
+router.get('/:id/podcast-lists', auth, UserController.getPodcastLists)
 
-module.exports = router;
+router.get('/:id', auth, UserController.getUserById)
+
+router.post('/', auth, UserController.createUser)
+
+router.put('/:id', auth, UserController.updateUser)
+
+router.delete('/:id', auth, UserController.deleteUser)
+
+module.exports = router
